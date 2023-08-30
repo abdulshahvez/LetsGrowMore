63bf914ea004f26cc2481c662f1ec51c466f73fd
A.Q.Shahvez <113885062+abdulshahvez@users.noreply.github.com>
2023-08-31 00:07:47 +0530

    3 task addes

---
+++ calculator/cal.js
@@ -0,0 +1,24 @@
+let string="";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+     if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('.bar').value=string;
+     }
+     else if(e.target.innerHTML == 'C'){
+        string='';
+        document.querySelector('.bar').value=string;
+     }
+     else if(e.target.innerHTML == 'X'){
+        string=string.substring(0,string.length-1);
+        document.querySelector('.bar').value=string;
+     }
+        else{
+            console.log(e.target)
+        string= string+ e.target.innerHTML;
+        document.querySelector('.bar').value=string;
+        }
+    })
+
+})
